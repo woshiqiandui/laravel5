8400726e066a225106112d815e48a536dd5fc915
dui <772595674@qq.com>
2016-12-12 22:34:29 +0800

    Initial commit

---
+++ app/Http/routes.php
@@ -0,0 +1,117 @@
+<?php
+
+//use Illuminate\Http\Request;
+///*
+//|--------------------------------------------------------------------------
+//| Application Routes
+//|--------------------------------------------------------------------------
+//|
+//| Here is where you can register all of the routes for an application.
+//| It's a breeze. Simply tell Laravel the URIs it should respond to
+//| and give it the controller to call when that URI is requested.
+//|
+//*/
+//
+////requestedRoute::get('/', function () {
+////    $tasks = Task::orderBy('created_at', 'asc')->get();
+////
+////    return view('tasks', [
+////        'tasks' => $tasks
+////    ]);
+////});
+///**
+// * Add New Task
+// */
+//
+//    Route::get('/', function () {
+//        return view('tasks', [
+//            'tasks' => Task::orderBy('created_at', 'asc')->get()
+//        ]);
+//    });
+//
+//Route::post('/task', function (Request $request) {
+//    $validator = Validator::make($request->all(), [
+//        'name' => 'required|max:255',
+//    ]);
+//
+//    if ($validator->fails()) {
+//        return redirect('/')
+//            ->withInput()
+//            ->withErrors($validator);
+//    }
+//
+//    $task = new Task;
+//    $task->name = $request->name;
+//    $task->save();
+//
+//    return redirect('/');
+//});
+///**
+// * Delete Task
+// */
+//Route::delete('/task/{task}', function (Task $task) {
+//    //
+//});
+//
+//Route::get('home', 'HomeController@index');
+//
+//Route::controllers([
+//	'auth' => 'Auth\AuthController',
+//	'password' => 'Auth\PasswordController',
+//]);
+
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+use App\Task;
+use Illuminate\Http\Request;
+
+Route::group(['middleware' => ['web']], function () {
+    /**
+     * Show Task Dashboard
+     */
+    Route::get('/', function () {
+        return view('tasks', [
+            'tasks' => Task::orderBy('created_at', 'asc')->get()
+        ]);
+    });
+
+    /**
+     * Add New Task
+     */
+    Route::post('/task', function (Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        $task = new Task;
+        $task->name = $request->name;
+        $task->save();
+
+        return redirect('/');
+    });
+
+    /**
+     * Delete Task
+     */
+    Route::delete('/task/{id}', function ($id) {
+        Task::findOrFail($id)->delete();
+
+        return redirect('/');
+    });
+});
